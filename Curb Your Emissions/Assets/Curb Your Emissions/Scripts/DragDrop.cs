aacd8a3ebd737960cb361a9d5953f8db5a2058a4
wlawson2 <wlawson2@ufl.edu>
2021-11-15 10:50:48 -0500

    Combined drag and drop with grid

--- Curb Your Emissions/Assets/Curb Your Emissions/Scripts/DragDrop.cs
+++ Curb Your Emissions/Assets/Curb Your Emissions/Scripts/DragDrop.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragDrop : MonoBehaviour
-{
-    void OnMouseDrag() {
-        transform.position = GetMousePos(); 
+public class DragDrop : MonoBehaviour {
+    private Vector3 dragOffset;
+
+    void OnMouseDown() {
+        dragOffset = transform.position - GetMousePos();
     }
+
+    void OnMouseDrag() {
+        transform.position = GetMousePos() + dragOffset;  
+    } 
+
 
     Vector3 GetMousePos() {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
